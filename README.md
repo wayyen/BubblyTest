71a02f16f07e4caa864904f5b0ac41916e6461d9
Chen Way Yen <wayyen@gmail.com>
2013-09-20 23:05:51 +0800

    initial import

---
+++ README.md
@@ -0,0 +1,10 @@
+AtoI
+----
+
+Convert numeric string to integers.
+
+To execute:
+
+    $ chmod +x atoi
+    $ ./atoi 1234
+
---
+++ src/com/example/test/AtoI.java
@@ -0,0 +1,83 @@
+package com.example.test;
+
+public class AtoI {
+    int result = 0;
+    String input = "";
+    int sign = 1;
+    int radix = 10;
+
+    public static void main(String... args) {
+        for (String arg : args) {
+            AtoI a = new AtoI(arg);
+            System.out.println("result: " +(a.convertStringToInt()));
+        }
+    }
+
+    public AtoI(String input) {
+        this.input = input;
+        this.radix = 10;
+    }
+
+    private int convertStringToInt() {
+        if (input == null) {
+            throw new NumberFormatException("null string.");
+        }
+        if (input.isEmpty()) {
+            throw new NumberFormatException("empty string.");
+        }
+        int i=0;
+        int digit = 0;
+        while (i < input.length()) {
+            if (i==0) { // first char
+                if (input.charAt(i)=='-') {
+                    // set negative here
+                    sign = -1;
+                } else if ((digit=charToInt(input.charAt(i))) != -1) {
+                    result += digit;
+                } else {
+                    throw new NumberFormatException("invalid string 1.");
+                }
+            } else {
+                // process the rest
+                if ((digit=charToInt(input.charAt(i))) != -1 ) {
+                    result = result * radix + digit; 
+                } else {
+                    throw new NumberFormatException("invalid string 2.");
+                }
+            }
+            i++;
+        }
+        return sign*result;
+    }
+
+    /*
+    * This is a strip-down replacement for Character.digit()
+    * and it only supports radix of 10. 
+    */
+    private int charToInt(char ch) { 
+        switch (ch) {
+            case '0':
+                return 0;
+            case '1':
+                return 1;
+            case '2':
+                return 2;
+            case '3':
+                return 3;
+            case '4':   
+                return 4;
+            case '5':   
+                return 5;
+            case '6':   
+                return 6;
+            case '7':   
+                return 7;
+            case '8':   
+                return 8;
+            case '9':   
+                return 9;
+            default:
+                return -1;
+        }
+    }
+}

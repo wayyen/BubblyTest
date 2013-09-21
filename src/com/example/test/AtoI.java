8c2b5f67aaa93eadb841f61df1d7028a8c2c7791
Chen Way Yen <wayyen@gmail.com>
2013-09-21 10:16:47 +0800

    use '0' codepoint to derive digits in charToInt instead of a code map

--- src/com/example/test/AtoI.java
+++ src/com/example/test/AtoI.java
@@ -69,34 +69,15 @@ public class AtoI {
         return sign*result;
     }
 
-    /*
-    * This is a strip-down replacement for Character.digit()
-    * and it only supports radix of 10. 
-    */
-    public static int charToInt(char ch) { 
-        switch (ch) {
-            case '0':
-                return 0;
-            case '1':
-                return 1;
-            case '2':
-                return 2;
-            case '3':
-                return 3;
-            case '4':   
-                return 4;
-            case '5':   
-                return 5;
-            case '6':   
-                return 6;
-            case '7':   
-                return 7;
-            case '8':   
-                return 8;
-            case '9':   
-                return 9;
-            default:
-                return -1;
+    /**
+     * Converts a character to base10 digit
+     * Supports only radix = 10
+     */
+    public static int charToInt(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return ch - '0';
+        } else {
+            return -1;
         }
     }
 }
